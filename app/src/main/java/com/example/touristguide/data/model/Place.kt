c4f634856fa478e68885c632ed98187315df6057
saroj thapa <kritanthapa224@gmail.com>
2025-07-15 19:06:42 +0545

    modify in place model

--- app/src/main/java/com/example/touristguide/data/model/Place.kt
+++ app/src/main/java/com/example/touristguide/data/model/Place.kt
@@ -1,4 +1,11 @@
 package com.example.touristguide.data.model
 
 // Placeholder for Place data model
-class Place 
+class Place (
+    val id: String = "",
+    val name: String = "",
+    val address: String = "",
+    val category: String = "",
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0
+)
