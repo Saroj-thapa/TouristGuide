5c0b26c6154be21a177a1ddbf5a01c50001b9500
saroj thapa <kritanthapa224@gmail.com>
2025-07-15 20:31:33 +0545

    adding the dependencies of weather

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -123,4 +123,9 @@ dependencies {
 
     // Google Sign-In
     implementation("com.google.android.gms:play-services-auth:21.1.0")
+
+    // Retrofit Api
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
 }
--- app/src/main/java/com/example/touristguide/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/touristguide/data/repository/WeatherRepository.kt
@@ -1,9 +1,19 @@
 package com.example.touristguide.data.repository
 
-import com.example.touristguide.data.network.RetrofitInstance
+import com.example.touristguide.network.OpenWeatherService
+import com.example.touristguide.network.OneCallResponse
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class WeatherRepository {
-    private val apiKey = "e2eb509886583ef39095ea03aae28ad1"
-    fun get7DayForecast(lat: Double, lon: Double) =
-        RetrofitInstance.weatherApi.get7DayForecast(lat, lon, apiKey = apiKey)
+    private val apiKey = "7524a8ef33c381084fef9f7871b4306c"
+    private val weatherApi: OpenWeatherService by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(OpenWeatherService::class.java)
+    }
+    suspend fun get7DayForecast(lat: Double, lon: Double): OneCallResponse =
+        weatherApi.getOneCallForecast(lat, lon, apiKey = apiKey)
 }
