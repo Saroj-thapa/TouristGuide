5b4308e13003164672983cb804ffeafc50c1d228
saroj thapa <kritanthapa224@gmail.com>
2025-07-15 20:52:00 +0545

    weather repsosity to fetcg data

--- app/src/main/java/com/example/touristguide/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/touristguide/data/repository/WeatherRepository.kt
@@ -1,19 +1,28 @@
 package com.example.touristguide.data.repository
 
-import com.example.touristguide.network.OpenWeatherService
-import com.example.touristguide.network.OneCallResponse
+import com.example.touristguide.data.network.WeatherService
+import com.example.touristguide.data.model.ForecastResponse
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class WeatherRepository {
     private val apiKey = "7524a8ef33c381084fef9f7871b4306c"
-    private val weatherApi: OpenWeatherService by lazy {
+    private val weatherApi: WeatherService by lazy {
         Retrofit.Builder()
             .baseUrl("https://api.openweathermap.org/data/2.5/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(OpenWeatherService::class.java)
+            .create(WeatherService::class.java)
     }
-    suspend fun get7DayForecast(lat: Double, lon: Double): OneCallResponse =
-        weatherApi.getOneCallForecast(lat, lon, apiKey = apiKey)
+    fun get5DayForecastByCoords(lat: Double, lon: Double) =
+        weatherApi.get5DayForecastByCoords(lat, lon, apiKey = apiKey)
+
+    fun get5DayForecastByCity(city: String) =
+        weatherApi.get5DayForecastByCity(city, apiKey = apiKey)
+
+    fun getCurrentWeatherByCoords(lat: Double, lon: Double) =
+        weatherApi.getCurrentWeatherByCoords(lat, lon, apiKey = apiKey)
+
+    fun getCurrentWeatherByCity(city: String) =
+        weatherApi.getCurrentWeatherByCity(city, apiKey = apiKey)
 }
